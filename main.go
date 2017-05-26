587bd3542e94165f720b21d40d30092a26d070d4
charles-d-burton <charles.d.burton@gmail.com>
2017-05-26 09:25:52 -0600

    Added Gin framework and wrapped program in REST

--- main.go
+++ main.go
@@ -1,42 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"bytes"
 	"log"
-	"os"
 
 	"github.com/charles-d-burton/gphoto2go"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Printf("Hello world.\n")
 	camera := new(gphoto2go.Camera)
 	err := camera.Init()
 	if err > 0 {
 		log.Println(err)
 	}
-	cameraFilePath, err := camera.TriggerCaptureToFile()
-	cameraFileReader := camera.FileReader(cameraFilePath.Folder, cameraFilePath.Name)
-	os.Mkdir("/tmp/testing", os.FileMode(0777))
-	fileWriter, err1 := os.Create("/tmp/testing/test.jpg")
-	if err1 != nil {
-		log.Println(err)
-	}
-	io.Copy(fileWriter, cameraFileReader)
-	cameraFileReader.Close()
-	/*folders := camera.RListFolders("/")
-	for _, folder := range folders {
-		files, _ := camera.ListFiles(folder)
-		for _, fileName := range files {
-			cameraFileReader := camera.FileReader(folder, fileName)
-			os.Mkdir("/tmp/testing", os.FileMode(0777))
-			fileWriter, err := os.Create("/tmp/testing/" + fileName)
-			if err != nil {
-				log.Println(err)
-			}
-			io.Copy(fileWriter, cameraFileReader)
-			cameraFileReader.Close()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/shot", func(c *gin.Context) {
+		cameraFilePath, err := camera.TriggerCaptureToFile()
+		if err > 0 {
+			cameraFileReader := camera.FileReader(cameraFilePath.Folder, cameraFilePath.Name)
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(cameraFileReader)
+
+			c.Data(200, "image/jpeg", buf.Bytes())
 		}
-	}*/
+	})
+	r.Run()
 }
