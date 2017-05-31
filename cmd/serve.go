aee62ea88c3749a269730f9b0c2921f41bc74e88
charles-d-burton <charles.d.burton@gmail.com>
2017-05-31 09:36:17 -0600

    Update descriptions

--- cmd/serve.go
+++ cmd/serve.go
@@ -17,7 +17,6 @@ import (
 	"bytes"
 	"encoding/base64"
 	"errors"
-	"fmt"
 	"goshot/utility"
 	"log"
 	"net/http"
@@ -37,15 +36,10 @@ var (
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "start serving the camera",
+	Long: `Serve the application on the given port
+or default to 8080`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("serve called")
 		log.Println("Args: ", strings.Join(args, ""))
 		serve()
 	},
